822b31c2f94dc2a05553e31b320ae28acf1a15ef
Santino Casolati <63636985+santinocasolati@users.noreply.github.com>
2024-02-11 01:06:13 -0300

    NavMeshSurface Created

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public UnityEvent OnRoundStart, OnRoundEnd;
+
+    [SerializeField] private NavMeshSurface walkableSurface;
 
     private void OnEnable()
     {
@@ -21,5 +24,10 @@ public class GameManager : MonoBehaviour
     private void RoundEnd()
     {
 
+    }
+
+    public void RoomPurchase()
+    {
+        walkableSurface.BuildNavMesh();
     }
 }
